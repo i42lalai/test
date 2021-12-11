5b9b8ec07b7e4def8b5e880d4e39d2c575444b63
modern <lakhanau@gmail.com>
2021-12-11 12:14:12 +0100

    Our third commit

---
+++ New_lesson2/excercise1.js
@@ -0,0 +1,56 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+// var number = 5;
+// var string = "Hello";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+// console.log(4/0);
+// console.log('string'*9);
+
+// let something;
+// console.log(something)
+
+// let person = {
+//     name: "John", 
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(person["name"]);
+
+// let arr = ['plum.pgn', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[0]); 
+
+//alert("Hello World")
+
+
+// let answer = +prompt("Are you 18", "Yes");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log( 4 + " - object");
+
+// let incr = 10,
+//     decr = 10;
+
+
+//     console.log(incr++);
+//     console.log(decr--);
+
+// console.log(5%2);
+// console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+    console.log(isChecked || !isClose);
+
